6c1b30a0868a6641da694d46b45a56b0f5a431bb
kelly-alphador <kellyalphador@gmail.com>
2025-07-19 22:16:53 +0300

    Jour 4

--- CSharp-List-Algorithms/Program.cs
+++ CSharp-List-Algorithms/Program.cs
@@ -9,36 +9,55 @@ namespace Travail_algo
     {
         static void Main(string[] args)
         {
-            List<int> list= new List<int> { 1,2,3,2,1};
-            ManipulationAlgorithms algo = new ManipulationAlgorithms();
-            SearchingAlgorithms algoSerach= new SearchingAlgorithms();
-            var doublons=algoSerach.TrouverDoublons(list);
-            foreach (int i in doublons) 
+            List<int> list = new List<int> { 1, 2, 3, 1, 4, 1 };
+            List<string> listString = new List<string> { "Alpha", "Brinda", "GAGA", "JEnny", "Olivia" };
+            List<Personne> Person = new List<Personne>()
             {
-                Console.WriteLine(i);
-            }
-            /* Liste d'entiers
-            List<int> intList = new List<int> { 1, 2, 3, 4 };
-            intList = algo.Reverse(intList);
-            Console.WriteLine("Reverse int: " + string.Join(", ", intList));
+                new Personne{Nom="Alpha",Age=21},
+                new Personne{Nom="Olivia",Age=30},
+                new Personne{Nom="Jose",Age=41},
+                new Personne{Nom="Marthinien",Age=42}
+            };
+            List<Employe> employes = new List<Employe>
+            {
+                new Employe { id = 1, Nom = "Alice", Departement = "Informatique", Salaire = 3200.50 },
+                new Employe { id = 2, Nom = "Bruno", Departement = "Ressources Humaines", Salaire = 2800.00 },
+                new Employe { id = 3, Nom = "Catherine", Departement = "Comptabilité", Salaire = 3100.75 },
+                new Employe { id = 4, Nom = "David", Departement = "Informatique", Salaire = 4000.00 },
+                new Employe { id = 5, Nom = "Emma", Departement = "Marketing", Salaire = 2700.20 },
+                new Employe { id = 6, Nom = "Farah", Departement = "Ventes", Salaire = 2600.00 },
+                new Employe { id = 7, Nom = "Georges", Departement = "Comptabilité", Salaire = 2950.40 },
+                new Employe { id = 8, Nom = "Hassan", Departement = "Ventes", Salaire = 2500.00 },
+                new Employe { id = 9, Nom = "Isabelle", Departement = "Informatique", Salaire = 4200.00 },
+                new Employe { id = 10, Nom = "Julien", Departement = "Marketing", Salaire = 3000.00 }
+            };
 
-            // Liste de caractères
-            List<char> charList = new List<char> { 'a', 'b', 'c', 'd' };
-            charList = algo.Reverse(charList);
-            Console.WriteLine("Reverse char: " + string.Join(", ", charList));
+            List<Produit> produits = new List<Produit>
+            {
+                new Produit { Nom = "Ordinateur", prix = 750 },
+                new Produit { Nom = "Clavier", prix = 45 },
+                new Produit { Nom = "Souris", prix = 25 },
+                new Produit { Nom = "Écran", prix = 150 },
+                new Produit { Nom = "Imprimante", prix = 120 },
+                new Produit { Nom = "Casque", prix = 60 },
+                new Produit { Nom = "Webcam", prix = 80 },
+                new Produit { Nom = "Disque dur", prix = 100 },
+                new Produit { Nom = "Chargeur", prix = 30 },
+                new Produit { Nom = "Tapis de souris", prix = 10 }
+            };
 
-            // Liste de chaînes
-            List<string> stringList = new List<string> { "un", "deux", "trois", "quatre" };
-            stringList = algo.Reverse(stringList);
-            Console.WriteLine("Reverse string: " + string.Join(", ", stringList));
-
-            // Liste d'objets
-            List<object> objectList = new List<object> { 1, "deux", 3.0, '4' };
-            objectList = algo.Reverse(objectList);
-            Console.Write("Reverse object: ");
-            foreach (var item in objectList)
-                Console.Write(item + " ");
-            Console.WriteLine();*/
+            ManipulationAlgorithms Algo =new ManipulationAlgorithms();
+            //Algo.SupprimerOccurrences(list, 1);
+            // Algo.AjouterSiAbsent(list, 1);
+            //Algo.AjouterSiAbsent(list, 5);
+            //Algo.AjouterAuMilieu(list, 5);
+            //Algo.RemplacerValeurs(list, 1, 10);
+            //Algo.SupprimerInferieurOuEgal(list, 5);
+            //Algo.ObtenirNombresPairs(list);
+            //Algo.ObtenirChainesCommencantParA(listString);
+            //Algo.ObtenirPersonnesAgeesDePlusDe30(Person);
+            Algo.ObtenirProduitsMoinsDe50(produits);
+            Algo.ObtenirEmployesDuDepartementVente(employes);
         }
     }
 }
